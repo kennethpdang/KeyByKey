999fde03ce6fccf3c01c93aea63d940db990092d
Kenneth Dang <kennethpdang@gmail.com>
2026-03-07 14:51:56 -0600

    refactor: Clean up schema formatting in collection and flashcard models

--- backend/models/collectionModel.js
+++ backend/models/collectionModel.js
@@ -2,21 +2,17 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const collectionSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    description: {
-        type: String,
-        required: true,
-        minlength: 10,
-        maxlength: 750
-    },
-    flashcards: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Flashcard'
-    }]
+	title: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	description: {
+		type: String,
+		required: true,
+		minlength: 10,
+		maxlength: 750
+	}
 }, { timestamps: true });
 
 module.exports = mongoose.model('Collection', collectionSchema);
--- backend/models/flashcardModel.js
+++ backend/models/flashcardModel.js
@@ -3,82 +3,82 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CellSchema = new Schema({
-    text: {
-        type: String,
-        default: "",
-        required: true
-    },
-    prefilled: {
-        type: Boolean,
-        default: false
-    }
-}, {_id: false});
+	text: {
+		type: String,
+		default: "",
+		required: true
+	},
+	prefilled: {
+		type: Boolean,
+		default: false
+	}
+}, { _id: false });
 
 const TableSchema = new Schema({
-    rows: {
-        type: Number,
-        min: 1,
-        max: 7,
-        required: true
-    },
-    cols: {
-        type: Number,
-        min: 1,
-        max: 7,
-        required: true
-    },
-    cells: {
-        type: [[CellSchema]],
-        required: true
-    }
+	rows: {
+		type: Number,
+		min: 1,
+		max: 7,
+		required: true
+	},
+	cols: {
+		type: Number,
+		min: 1,
+		max: 7,
+		required: true
+	},
+	cells: {
+		type: [[CellSchema]],
+		required: true
+	}
 }, { _id: false });
 
 const flashcardSchema = new Schema({
-    header: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        default: '',
-        required: function () {
-            return this.type !== 'table';
-        }
-    },
-    collectionName: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Collection',
-        required: true
-    },
-    spacedRepitition: {
-        type: Number,
-        required: true,
-        enum: [1, 2, 4, 8, 16, 32, 64, 128, 256, 365],
-        default: 1
-    },
-    masteredCount: {
-        type: Number,
-        default: 0 
-    },
-    nextDueAt: {
-        type: Date,
-        default: null
-    },
-    lastReviewedAt: {
-        type: Date,
-        default: null
-    },
-    lastMasteredAt: {
-        type: Date,
-        default: null
-    },
-    // String flashcard or table flashcard:
-    type: {
-        type: String,
-        enum: ['text', 'table'],
-        default: 'text'
-    },
-    table: { type: TableSchema, default: null }
+	header: {
+		type: String,
+		required: true
+	},
+	content: {
+		type: String,
+		default: '',
+		required: function () {
+			return this.type !== 'table';
+		}
+	},
+	collection: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Collection',
+		required: true,
+		index: true
+	},
+	spacedRepetition: {
+		type: Number,
+		required: true,
+		default: 1
+	},
+	masteredCount: {
+		type: Number,
+		default: 0
+	},
+	nextDueAt: {
+		type: Date,
+		default: null,
+		index: true
+	},
+	lastReviewedAt: {
+		type: Date,
+		default: null
+	},
+	lastMasteredAt: {
+		type: Date,
+		default: null
+	},
+	type: {
+		type: String,
+		enum: ['text', 'table'],
+		default: 'text'
+	},
+	table: { type: TableSchema, default: null }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Flashcard', flashcardSchema);
